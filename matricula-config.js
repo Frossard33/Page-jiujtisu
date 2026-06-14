9ea9476c19b96b00a1ef5dc7e2d8649f05f92204
Victor Frossard <victorfrossard@hotmail.com>
2026-06-14 16:15:40 -0300

    Publica matricula integrada ao CRM

---
+++ matricula-config.js
@@ -0,0 +1,6 @@
+window.MATRICULA_CONFIG = {
+  apiUrl: "https://jiu-crm-backend.onrender.com/api/matriculas-online/",
+  academySlug: "gilson-batista",
+  academyEmail: "gilsonbatista7@hotmail.com",
+  contractVersion: "v3-2026-06"
+};
