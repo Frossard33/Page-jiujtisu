2d6e4045fc435fbeb085a68875ba6faea4f542ed
Victor Frossard <victorfrossard@hotmail.com>
2026-06-14 17:04:30 -0300

    Automatiza assinatura do professor nos contratos

--- matricula-config.js
+++ matricula-config.js
@@ -2,5 +2,6 @@ window.MATRICULA_CONFIG = {
   apiUrl: "https://jiu-crm-backend.onrender.com/api/matriculas-online/",
   academySlug: "gilson-batista",
   academyEmail: "gilsonbatista7@hotmail.com",
-  contractVersion: "v4-2026-06"
+  professorName: "Gilson Batista",
+  contractVersion: "v5-2026-06"
 };
